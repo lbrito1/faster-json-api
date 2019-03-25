b26fbc2f9a16ebdecb6d6dc922750a369e7406ab
Leonardo Brito <lbrito@gmail.com>
2019-03-25 16:43:37 -0300

    Adds nested table in json agg query.

--- app/serializers/party_serializer.rb
+++ app/serializers/party_serializer.rb
@@ -12,16 +12,28 @@ module PartySerializer
   def base_sql
     <<-SQL.gsub(/\n/, '')
       SELECT
-      json_agg(
-        json_build_object(
-          'id', id,
-          'name', name,
-          'description', description,
-          'starts_at', starts_at,
-          'ends_at', ends_at
+        json_agg(
+          json_build_object(
+            'id', parties.id,
+            'name', parties.name,
+            'description', parties.description,
+            'starts_at', parties.starts_at,
+            'ends_at', parties.ends_at,
+            'sweepstakes', s.json_agg
+          )
         )
-      )
       FROM parties
+      LEFT JOIN (
+        SELECT
+            party_id,
+            json_agg(
+               json_build_object(
+                'name', name
+               )
+            )
+        FROM sweepstakes
+        GROUP BY party_id
+        ) s ON s.party_id = parties.id
     SQL
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,17 @@
-Party.create(
+p = Party.create(
   name: 'My awesome party',
   description: 'Incredible awesomeness',
   starts_at: Time.new(2022, 1, 1, 20, 00),
   ends_at: Time.new(2022, 1, 1, 23, 00)
 )
+
+p.sweepstakes.create(
+  name: 'My awesome Sweepstake',
+  description: 'Pure incredibleness'
+)
+
+p.sweepstakes.create(
+  name: 'My second awesome Sweepstake',
+  description: 'Purer incredibleness'
+)
+
